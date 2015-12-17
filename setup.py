838307f7ff449524ac1645353ee9181b0662a57b
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2015-12-17 16:46:28 +0100

    New runCoalFLUT script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='coalFLUT',
+        version='0.0.1',
+        packages=['test', 'coalFLUT'],
+        scripts = ['scripts/runCoalFLUT'],
+        url='',
+        license='',
+        author='Michele Vascellari',
+        author_email='Michele.Vascellari@vtc.tu-freiberg.de',
+        description='Generate FLUT for coal combustion musing multi-dimensional flamelet'
+)
