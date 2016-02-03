eb1a8e4e7533d17114703087888068d55f0759e9
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2016-02-03 12:47:28 +0100

    New multilevel total enthalpy distribution for oxidizer too.

--- coalFLUT/__init__.py
+++ coalFLUT/__init__.py
@@ -54,7 +54,8 @@ def runUlf(ulf_settings, Y, chist, Hnorm, fuel, ox):
     pressure = float(runner['PRESSURE'])
 
     fuel['T'] = 300
-    # dumb fuel temperature equilibrium necessary only for the stoichiomtric
+    ox['T'] = 300
+    # dumb fuel and oxidizer temperature equilibrium necessary only for the stoichiomtric
     # conditions
     eq = equilibrium.EquilibriumSolution(fuel=fuel, oxidizer=ox, mechanism=runner['MECHANISM'])
     runner.set('ZST', eq.z_stoich())
@@ -67,18 +68,24 @@ def runUlf(ulf_settings, Y, chist, Hnorm, fuel, ox):
         runner.set('OXIVAL{}'.format(i), ox['Y'].get(sp, 0))
 
     runner.set('CHIST', chist)
-    runner.set('TOXIDIZER', ox['T'])
-    H = fuel['H'].min() + Hnorm * (fuel['H'].max() - fuel['H'].min())
-    runner.set('TFUEL', calc_tf(eq.gas, H, pressure, fuel['Y']))
 
-    try:
-        print("Run {}".format(ulf_basename))
-        runner.run()
-        shutil.copy(ulf_basename_run+'final.ulf', ulf_result)
-        print("End run {}".format(ulf_basename))
-    except:
-        print("Error running {}".format(ulf_basename))
-        return None
+    #Hf = fuel['H'].min() + Hnorm * (fuel['H'].max() - fuel['H'].min())
+    runner.set('TFUEL', calc_tf(eq.gas, fuel['H'].min() + Hnorm * (fuel['H'].max() - fuel['H'].min()),
+                                pressure, fuel['Y']))
+    Ho = ox['H'].min() + Hnorm * (ox['H'].max() - ox['H'].min())
+    runner.set('TOXIDIZER', calc_tf(eq.gas, Ho, pressure, ox['Y']))
+    print("Run {}".format(ulf_basename))
+    runner.run()
+    shutil.copy(ulf_basename_run+'final.ulf', ulf_result)
+    print("End run {}".format(ulf_basename))
+    #try:
+    #    print("Run {}".format(ulf_basename))
+    #    runner.run()
+    #    shutil.copy(ulf_basename_run+'final.ulf', ulf_result)
+    #    print("End run {}".format(ulf_basename))
+    #except:
+    #    print("Error running {}".format(ulf_basename))
+    #    return None
     if not os.path.exists(backup_dir):
         os.mkdir(backup_dir)
     for f in glob.glob(ulf_basename_run+ "*"):
@@ -180,38 +187,50 @@ class coalFLUT(ulf.UlfDataSeries):
             inp = yaml.load(f)
         self.mechanism = inp['mechanism']
         self.gas = cantera.Solution(self.mechanism)
-        self.coal = inp['coal']
         # TODO volatiles can be defined as C, H, O
-        self.volatiles = inp['coal']['volatiles']
-        self.volatiles['Y'] = normalize(self.volatiles['Y'])
-        self.oxidizer = inp['oxidizer']
-        if 'Y' in self.oxidizer:
-            self.oxidizer['Y'] = normalize(self.oxidizer['Y'])
+        self.volatiles = {}
+        self.volatiles['Y'] = normalize(inp['volatiles']['Y'])
+        self.volatiles['T'] = read_dict_list(**inp['volatiles']['T'])
+
+        self.oxidizer = {}
+        #self.oxidizer[] = inp['oxidizer']
+        if 'Y' in inp['oxidizer']:
+            self.oxidizer['Y'] = normalize(inp['oxidizer']['Y'])
             self.oxidizer['X'] = convert_mass_to_mole(self.oxidizer['Y'], self.gas)
         else:
-            self.oxidizer['X'] = normalize(self.oxidizer['X'])
+            self.oxidizer['X'] = normalize(inp['oxidizer']['X'])
             self.oxidizer['Y'] = convert_mole_to_mass(self.oxidizer['X'], self.gas)
+        self.oxidizer['T'] = read_dict_list(**inp['oxidizer']['T'])
         # define self.Y
         self.chist = read_dict_list(**inp['mixture_fraction']['chist'])
         self.Y = read_dict_list(**inp['mixture_fraction']['Y'])
-        self.Tf = read_dict_list(**inp['coal']['T'])
-        n_H = len(self.Tf)
+
+        n_H = len(self.volatiles['T'])
+        # check if volatile and oxidizer temperature levels are the same
+        # if not use volatiles as refence number
+        if len(self.oxidizer['T']) != n_H:
+            self.oxidizer['T'] = np.linspace(self.oxidizer['T'].min(), self.oxidizer['T'].max(),
+                                             n_H)
+
         self.ulf_settings = inp['ulf']
         runner = ulf.UlfRun(self.ulf_settings['basename']+".ulf", self.ulf_settings['solver'])
         pressure = float(runner['PRESSURE'])
         self.chargas = self._define_chargas()
+        self.chargas['T'] = self.volatiles['T']
 
         # define H normalized between 0 and 1
         # Hnorm = 0 corresponds to Tf min
         # Hnorm = 1 corresponds to Tf max
-        self.Hnorm = (self.Tf - self.Tf.min())/(self.Tf.max()-self.Tf.min())
+        #self.Hnorm = (self.Tf - self.Tf.min())/(self.Tf.max()-self.Tf.min())
+        self.Hnorm = np.linspace(0, 1, n_H)
 
-        H = [np.linspace(calc_hf(self.gas, self.Tf.min(), pressure, fuel['Y']),
-                         calc_hf(self.gas, self.Tf.max(), pressure, fuel['Y']),
+        H = [np.linspace(calc_hf(self.gas, fuel['T'].min(), pressure, fuel['Y']),
+                         calc_hf(self.gas, fuel['T'].max(), pressure, fuel['Y']),
                          n_H)
-             for fuel in [self.volatiles, self.chargas]]
+             for fuel in [self.volatiles, self.chargas, self.oxidizer]]
         self.volatiles['H'] = H[0]
         self.chargas['H'] = H[1]
+        self.oxidizer['H'] = H[2]
 
         self.z_points = inp['mixture_fraction']['Z']['points']
 
