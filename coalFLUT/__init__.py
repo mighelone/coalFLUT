6f79f9487e4c5a47e515f01427ca420d2a63d1f0
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2017-03-23 21:59:08 +0100

    start restructuring

--- coalFLUT/__init__.py
+++ coalFLUT/__init__.py
@@ -5,5 +5,5 @@ coalFLUT
 from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
 
-from .flut import CoalFLUT
-from .equilibrium import CoalFLUTEq
+#from .flut import CoalFLUT
+#from .equilibrium import CoalFLUTEq
--- setup.py
+++ setup.py
@@ -10,5 +10,11 @@ setup(
     author='Michele Vascellari',
     author_email='Michele.Vascellari@vtc.tu-freiberg.de',
     description=('Generate FLUT for coal combustion using'
-                 ' multi-dimensional flamelet')
+                 ' multi-dimensional flamelet'),
+    install_requires=[
+        'pytest',
+        'termcolor',
+        'autologging',
+        'pyFLUT>2.4.0',
+        'mock']
 )
