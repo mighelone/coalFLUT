d67a78ec1c9c043bec9d2a49d8f139ddedd434b1
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2017-03-29 16:28:24 +0200

    New CoalPremixFLUT class

--- coalFLUT/premix/__init__.py
+++ coalFLUT/premix/__init__.py
@@ -7,6 +7,12 @@ from __future__ import print_function, unicode_literals
 from ..abstract import AbstractCoalFLUT
 from autologging import logged
 
+import numpy as np
+import cantera
+import pyFLUT
+import pyFLUT.ulf
+import shutil
+
 
 @logged
 class CoalPremixFLUT(AbstractCoalFLUT):
@@ -14,5 +20,84 @@ class CoalPremixFLUT(AbstractCoalFLUT):
     Coal FLUT using premix flames
     """
 
-    _files = ()
-    pass
+    _along = 'X'
+    _key_variable = 'X'
+    _parameter_names = ['vel_ratio', 'Z', 'Y']
+    _key_names_extra = ('tpatch_end', 'Z', 'T_fix', 'sl_guess')
+    _ulf_parameters_extra = ('tpatch_end', 'ulf_reference_bs', 'Le1',
+                             'T_fix', 'sl_guess')
+    _files = ('bs_setup.ulf',
+              'bs_template.ulf',
+              'fp_mixtureEntries.ulf',
+              'fp_new_template.ulf',
+              'fp_setup.ulf',
+              'hReactor_relax.ulf',
+              'ch4_smooke.xml',
+              'input.yml')
+
+    def run(self, n_p=1):
+
+        # the first time calculate solution for vel_ratio=1
+        parameters = self._parameter.copy()
+        parameters['vel_ratio'] = np.array([1.0])
+
+        self.__log.info(
+            'Start calculating freely propagating (fp) flames')
+        results_fp = self._run_parameters(n_p, parameters)
+
+        # sL is the array ZxY of the laminar flame speeds
+        sL = np.zeros_like((len(self.Z), len(self.Y)))
+        Z = self.Z.tolist()
+        Y = self.Y.tolist()
+        assert len(sL.ravel()) == len(results_fp), (
+            "Number of "
+            "results_fp different from YxZ")
+        for flame in results_fp:
+            i_Z = Z.index(flame.variables['Z'])
+            i_Y = Y.index(flame.variables['Z'])
+            sL[i_Z, i_Y] = flame['u'][0]
+
+        self.sL = sL
+
+        self.__log.info(
+            'Start calculating bs flames')
+        parameters = self._parameter.copy()
+        parameters['vel_ratio'] = parameters['vel_ratio'][:-1]
+        results_bs = self._run_parameters(n_p, parameters)
+
+        self.assemble_results(results_fp + results_bs)
+
+    def _run_set_runner(self, basename_calc, input_file, parameters):
+        # define the runner and set running parameters
+        self.__log.debug('Set runner %s', parameters)
+        if self.gas is None:
+            gas = cantera.Solution(self.mechanism)
+        else:
+            gas = self.gas
+        vel_ratio, Z, Y = parameters
+        # define the streams for the given Hnorm and Y
+        mix = self.mix_fuels(Y, 0, gas)
+        oxid = self.calc_temperature(self.oxidizer, 0, gas)
+
+        # get laminar flame speed
+
+        if vel_ratio < 1:
+            # use bs setup
+            shutil.copy(self.ulf_reference_bs, input_file)
+            sL = self.sL[
+                self.Z.tolist().index(Z),
+                self.Y.tolist().index(Y)] * vel_ratio
+        else:
+            sL = self.sl_guess
+
+        runner = pyFLUT.ulf.UlfRun(input_file, self.solver)
+        runner.set(self.keys['basename'], basename_calc)
+        runner.set(self.keys['sl_guess'], sL)
+        runner.set(self.keys['Z'], Z)
+        runner.set(self.keys['T_fix'], mix['T'] + self.T_fix)
+
+        self.__log.debug('Set species mix: %s', mix)
+        pyFLUT.utilities.set_species(runner, mix, oxid,
+                                     gas.species_names)
+
+        return runner
--- test/test_premix.py
+++ test/test_premix.py
@@ -1,31 +1,117 @@
 import pytest
+import os
+import shutil
+import glob
+import numpy as np
 
 from coalFLUT.premix import CoalPremixFLUT
+from pyFLUT.ulf import UlfRun
 
 
-settings = {'flut': {'c': 101,
-                     'pv': {'CO': 1, 'CO2': 1},
-                     'solver': 'OpenFOAM',
-                     'varZ': {'method': 'linspace', 'values': [0, 1, 3]},
-                     'variables': ['rho', 'T']},
-            'oxidizer': {'T': {'max': 500, 'min': 300}, 'X': {'N2': 0.79, 'O2': 0.21}},
-            'parameters': {'Hnorm': {'method': 'linspace', 'values': [0, 1, 2]},
-                           'Y': {'method': 'linspace', 'values': [0, 1, 3]},
-                           'chist': {'method': 'logspace', 'values': [-1, 1, 10]}},
-            'pressure': 101325,
-            'ulf': {'basename': 'test',
-                    'keys': {'Zst': 'ZST',
-                             'basename': 'RESULT_BASENAME',
-                             'chist': 'CHIST',
-                             'mechanism': 'MECHANISM',
-                             'n_grid': 'REFINE_TARGET',
-                             'pressure': 'PRESSURE'},
-                    'tpatch_end': 500,
-                    'mechanism': 'ch4_smooke.xml',
-                    'n_grid': 101,
-                    'par_format': '{:5.4f}',
-                    'restart': True,
-                    'solver': '/shared_home/vascella/Codes/ulf2/build/ulfrun',
-                    'ulf_reference': 'flamelet_setup.ulf'},
-            'volatiles': {'T': {'max': 600, 'min': 300},
-                          'Y': {'CH4': 0.5, 'H2': 0.2, 'N2': 0}}}
+settings = {
+    'parameters': {
+        'Hnorm': {'method': 'linspace', 'values': [0, 1, 5]},
+        'vel_ratio': {'method': 'linspace', 'values': [0.1, 1, 10]},
+        'Y': {'method': 'linspace', 'values': [0.7, 1.0, 31]},
+        'Z': {'method': 'linspace', 'points': 101, 'values': [0, 0.2, 21]}},
+    'flut': {
+        'c': {'points': 101},
+        'pv': {'CO': 1, 'CO2': 1},
+        'solver': 'OpenFOAM',
+        'varZ': {'method': 'linspace', 'values': [0, 1, 3]},
+        'variables': ['rho', 'T']},
+    'oxidizer': {'T': {'max': 710, 'min': 310},
+                 'X': {'N2': 0.21, 'O2': 0.79}},
+    'pressure': 101325,
+    'ulf': {'Le1': True,
+            'tpatch_end': 0.002,
+            'basename': 'cpremix',
+            'keys': {'basename': 'RESULT_BASENAME',
+                     'chist': 'CHIST',
+                     'mechanism': 'MECHANISM',
+                     'n_grid': 'REFINE_TARGET',
+                     'pressure': 'PRESSURE',
+                     'tpatch_end': 'T_PATCH_END',
+                     'sl_guess': 'SL_GUESS',
+                     'Z': 'Z_VALUE',
+                     'T_fix': 'TFIX'},
+            'mechanism': 'ch4_smooke.xml',
+            'n_grid': 270,
+            'par_format': '{:5.4f}',
+            'restart': False,
+            'T_fix': 20.0,
+            'sl_guess': 0.2,
+            'solver': '/shared_home/messig/ULF/build_opt/ulf.x',
+            'ulf_reference': 'fp_setup.ulf',
+            'ulf_reference_bs': 'bs_setup.ulf'},
+    'volatiles': {'T': {'max': 600, 'min': 310}, 'Y': {'CH4': 1}}}
+
+
+@pytest.fixture(scope='module')
+def test_path():
+    """Setup the path for testing CoalDiffusionFLUT"""
+    cwd = os.getcwd()
+    path = os.path.abspath(os.path.join(cwd, 'test/test_premix'))
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+    os.chdir(path)
+    CoalPremixFLUT.copy_files()
+    yield path
+
+    # teardown
+    os.chdir(cwd)
+    shutil.rmtree(path)
+
+
+@pytest.fixture
+def flut(test_path):
+    """Init CoalDiffusionFLUT"""
+    cflut = CoalPremixFLUT(settings)
+    yield cflut
+
+    # teardown
+    # remove intermediate files
+    [os.remove(f) for f in glob.iglob('inp_*.ulf')]
+    [os.remove(f)
+     for f in glob.iglob('{}_*.ulf'.format(cflut.basename))]
+
+
+def test_init(flut):
+    runner = UlfRun(flut.ulf_reference, flut.solver)
+    assert float(runner[flut.keys['tpatch_end']]) == flut.tpatch_end
+
+
+def test_run_set_runner_fp(flut):
+    """
+    test set_runner for vel_ratio = 1
+    """
+    parameters = (1, 0.1, 1)
+    basename_calc = flut.basename + flut.create_label(parameters) + '_run'
+    input_file = 'inp' + flut.create_label(parameters) + '_run'
+    shutil.copy(flut.ulf_reference, input_file)
+    runner = flut._run_set_runner(basename_calc, input_file, parameters)
+
+    assert float(runner[flut.keys['Z']]) == parameters[1]
+    assert runner[flut.keys['basename']] == basename_calc
+    # assert float(runner[flut.keys['T_fix']])
+    assert float(runner[flut.keys['sl_guess']]) == flut.sl_guess
+
+
+def test_run_set_runner_bs(flut):
+    '''
+    test set_runner for vel_ratio < 1
+    '''
+    parameters = (0.9, 0.1, 1)
+
+    sL = 0.5
+    flut.sL = np.ones((len(flut.Z), len(flut.Y)), dtype=float) * sL
+    basename_calc = flut.basename + flut.create_label(parameters) + '_run'
+    input_file = 'inp' + flut.create_label(parameters) + '_run'
+    shutil.copy(flut.ulf_reference, input_file)
+    runner = flut._run_set_runner(basename_calc, input_file, parameters)
+
+    assert float(runner[flut.keys['Z']]) == parameters[1]
+    assert runner[flut.keys['basename']] == basename_calc
+    # assert float(runner[flut.keys['T_fix']])
+    assert float(runner[flut.keys['sl_guess']]) == sL * parameters[0]
