79e95f02d88fafb7c64cf5ed099bea33c61adcd6
Michele Vascellari <Michele.Vascellari@vtc.tu-freiberg.de>
2017-09-27 12:24:16 +0200

    Add diffusion and premix submodules to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='coalFLUT',
     version='1.1.0',
-    packages=['coalFLUT', 'coalPFLUT', 'coal_calculation'],
+    packages=['coalFLUT', 'coalPFLUT', 'coal_calculation',
+              'coalFLUT.diffusion', 'coalFLUT.premix'],
     scripts=['scripts/runCoalFLUT'],
     url='',
     license='',
@@ -11,6 +12,7 @@ setup(
     author_email='Michele.Vascellari@vtc.tu-freiberg.de',
     description=('Generate FLUT for coal combustion using'
                  ' multi-dimensional flamelet'),
+
     install_requires=[
         'pytest',
         'termcolor',
